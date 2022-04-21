89a7c8d843a52b51a646aa707c91c928a64f662a
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2022-04-21 11:17:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
-from pyrogram import Client, filters
+from pyrogram import Client, idle, filters
 import pikudhaoref
 
 api_id: int = 0  # Put your API ID here
 api_hash: str = ""  # Put your API hash here
 bot_token: str = ""  # Put your bot token here
+
 
 app = Client(
     "redalertbot",
@@ -14,16 +15,16 @@ app = Client(
 chats_to_forward = []
 main_channel = "-100"
 
-client = pikudhaoref.SyncClient(update_interval=2)
+client = pikudhaoref.AsyncClient(update_interval=2, loop=app.loop)
 
 
 @app.on_message(filters.command("start"))
-def pvtmsg(c, m):
-    m.reply("הבוט נוצר על ידי @itayki ופועל בערוץ https://t.me/redalertil2021")
+async def pvtmsg(c, m):
+    await m.reply("הבוט נוצר על ידי @itayki ופועל בערוץ https://t.me/redalertil2021")
 
 
 @client.event()
-def on_siren(sirens):
+async def on_siren(sirens):
     for i in sirens:
         city_name = i.city.name.he
         city_countdown = i.city.countdown.he
@@ -31,13 +32,22 @@ def on_siren(sirens):
         if not i.city.zone.he:
             city_countdown = "לא ידוע"
 
-        a = app.send_message(
+        a = await app.send_message(
             main_channel,
             f"🔴 <b>צבע אדום</b>\n\n<b>עיר:</b> {city_name}\n<b>אזור:</b> {city_zone}\n<b>זמן:</b> {city_countdown}\n\n<b>ערוץ https://t.me/redalertil2021</b>",
             disable_web_page_preview=True,
         )
         for e in chats_to_forward:
-            a.forward(e)
+            await a.forward(e)
 
 
-app.run()
+async def main():
+    await app.start()
+    await client.initialize()
+    await idle()
+    client.closed = True
+    await client.http.session.close()
+    await app.stop()
+
+
+app.loop.run_until_complete(main())

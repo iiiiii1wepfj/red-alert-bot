ce3fc77272a0a849efc7befc1976582906d61ca7
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2022-08-07 13:33:54 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
-from pyrogram import Client, idle, filters
-import pikudhaoref
+from pyrogram import Client, filters
+import requests, time, threading, json
+
 
 api_id: int = 0  # Put your API ID here
 api_hash: str = ""  # Put your API hash here
@@ -15,39 +16,64 @@ app = Client(
 chats_to_forward = []
 main_channel = "-100"
 
-client = pikudhaoref.AsyncClient(update_interval=2, loop=app.loop)
+
+pikud_locations_data = (requests.get("https://www.tzevaadom.co.il/static/cities.json")).json()
+cities_data = pikud_locations_data["cities"]
+areas_data = pikud_locations_data["areas"]
+countdown_data = pikud_locations_data["countdown"]
 
 
 @app.on_message(filters.command("start"))
-async def pvtmsg(c, m):
-    await m.reply("הבוט נוצר על ידי @itayki ופועל בערוץ https://t.me/redalertil2021")
+def pvtmsg(c, m):
+    m.reply("הבוט נוצר על ידי @itayki ופועל בערוץ https://t.me/redalertilchannel")
 
 
-@client.event()
-async def on_siren(sirens):
-    for i in sirens:
-        city_name = i.city.name.he
-        city_countdown = i.city.countdown.he
-        city_zone = i.city.zone.he or "לא ידוע"
-        if not i.city.zone.he:
-            city_countdown = "לא ידוע"
+class siren_listener:
+    def __init__(self, callback):
+        self.callback = callback
+        self.running = True
+        self.last_data = ""
+        self.thread = threading.Thread(target=self.__listener__)
+        self.thread.start()
 
-        a = await app.send_message(
+    def stop(self):
+        self.running = False
+
+    def __listener__(self):
+        while self.running:
+            try:
+                time.sleep(2)
+                response = requests.get("https://www.oref.org.il/WarningMessages/Alert/alerts.json", headers={"X-Requested-With":"XMLHttpRequest","Referer":"https://www.oref.org.il/"})
+                res_content = (response.content).decode("utf-8-sig")
+                if len(res_content) > 4 and response.status_code == 200 and res_content != self.last_data:
+                    alert_data = json.loads(res_content)
+                    threading.Thread(target=self.callback, args=(alert_data,)).start()
+                    self.last_data = res_content
+            except:
+                pass
+
+
+def on_siren(sirens):
+    for i in sirens["data"]:
+        city_info = cities_data[i]
+        try:
+            countdown_for_the_city = countdown_data[str(city_info["countdown"])]["he"]
+        except:
+            countdown_for_the_city = "לא ידוע"
+        try:
+            area_for_the_city = areas_data[str(city_info["area"])]["he"]
+        except:
+            area_for_the_city = "לא ידוע"
+
+        a = app.send_message(
             main_channel,
-            f"🔴 <b>צבע אדום</b>\n\n<b>עיר:</b> {city_name}\n<b>אזור:</b> {city_zone}\n<b>זמן:</b> {city_countdown}\n\n<b>ערוץ https://t.me/redalertil2021</b>",
+            f"🔴 <b>צבע אדום</b>\n\n<b>עיר:</b> {i}\n<b>אזור:</b> {area_for_the_city}\n<b>זמן:</b> {countdown_for_the_city}\n\n<b>ערוץ https://t.me/redalertilchannel</b>",
             disable_web_page_preview=True,
         )
         for e in chats_to_forward:
-            await a.forward(e)
+            a.forward(e)
 
 
-async def main():
-    await app.start()
-    await client.initialize()
-    await idle()
-    client.closed = True
-    await client.http.session.close()
-    await app.stop()
-
-
-app.loop.run_until_complete(main())
+siren_listener_handler = siren_listener(on_siren)
+app.run()
+siren_listener_handler.stop()
